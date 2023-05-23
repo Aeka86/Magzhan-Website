550ff4e5366a94ea311745f8f1096691b87e2072
Aigerim Sagimbayeva <aeka86@aol.com>
2023-05-23 15:37:18 +0600

    some changes to html files

--- expeditions/exped.js
+++ expeditions/exped.js
@@ -1,12 +1,12 @@
-window.addEventListener('scroll', () => {
-    let content = document.querySelector('.hidden');
-    let contentPosition = content.getBoundingClientRect().top;
-    let screenPosition = window.innerHeight;
-    if (contentPosition < screenPosition) {
-        content.classList.add('show');
-    } else{
-        content.classList.remove('active');
-    }
-})
+window.addEventListener("scroll", () => {
+  let content = document.querySelector(".hidden");
+  let contentPosition = content.getBoundingClientRect().top;
+  let screenPosition = window.innerHeight;
+  if (contentPosition < screenPosition) {
+    content.classList.add("show");
+  } else {
+    content.classList.remove("active");
+  }
+});
 // button to TOP
-const toTop = () => window.scrollTo({top: 0, behavior: 'smooth'})
+const toTop = () => window.scrollTo({ top: 0, behavior: "smooth" });
